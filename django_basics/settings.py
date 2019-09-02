f463e8fdf8c7b88a75eb45d5405b70aa6f4429fc
Raja CSP Raman <raja.csp@gmail.com>
2019-09-02 19:23:38 -0400

    Login Logout page updated

--- django_basics/settings.py
+++ django_basics/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '&$z#rx60q_2$20l8-hvhg9(-0g!p9_1)bl-59@x&do$o@3#!rx'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -123,3 +123,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'blog-home'
--- users/views.py
+++ users/views.py
@@ -10,9 +10,9 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
 
-            messages.success(request, f'Account create for {username}')
+            messages.success(request, f'Your account has been created! You are now able to login ')
 
-            return redirect('blog-home')
+            return redirect('login')
 
     else:
         form = UserRegisterForm()
